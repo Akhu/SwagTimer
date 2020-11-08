bce0973ed2b71eaa93e1863c49592b01fbafdcc5
akhu <anthony.dacr@gmail.com>
2020-11-08 22:25:36 +0100

    Initial Commit

---
+++ SwagTimer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwagTimer
+//
+//  Created by Anthony Da Cruz on 08/11/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwagTimer/SwagTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwagTimerApp.swift
+//  SwagTimer
+//
+//  Created by Anthony Da Cruz on 08/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct SwagTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
